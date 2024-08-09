aaac09892f85fc927088664bece2e3ec2eeca4b9
mayorbyrne <kmoritz@gmail.com>
2024-08-08 22:23:08 -0500

    notify user if dart sdk is different

--- lua/dart-sdk-updates/init.lua
+++ lua/dart-sdk-updates/init.lua
@@ -1,70 +1,58 @@
-local M = {};
+local M = {}
 
 local getSDKVersion = function()
   local open = io.open
 
   local function read_file(path)
-      local file = open(path, "rb") -- r read mode and b binary mode
-      if not file then return nil end
-      local content = file:read "*a" -- *a or *all reads the whole file
-      file:close()
-      return content
+    local file = open(path, "rb") -- r read mode and b binary mode
+    if not file then
+      return nil
+    end
+    local content = file:read("*a") -- *a or *all reads the whole file
+    file:close()
+    return content
   end
 
-  return read_file("/Users/Kevin/Desktop/dart-sdk/version");
+  local dartLocation = io.popen("which dart")
+  if dartLocation == nil then
+    return nil
+  end
+
+  local dartPath = dartLocation:read("*a")
+
+  return read_file(vim.trim(dartPath:gsub("/bin/dart", "/version")))
 end
 
 local getLatestSdkVersion = function()
-  local http = require('socket.http')
-  -- local json = require("./json.lua");
-  local body = http.request("https://storage.googleapis.com/dart-archive/channels/stable/release/latest/VERSION");
+  local h = io.popen("curl -s https://storage.googleapis.com/dart-archive/channels/stable/release/latest/VERSION")
+  if h == nil then
+    return nil
+  end
+  local rawdata = h:read("all")
+  if rawdata == nil then
+    return nil
+  end
+  h:close()
+  local t = vim.json.decode(rawdata)
 
-  print(body);
+  return vim.inspect(t.version):gsub("\"", "")
 end
 
 M.setup = function()
-  local latestVersion = getLatestSdkVersion();
-  local version = getSDKVersion();
+  local latestVersion = getLatestSdkVersion()
+  local currVersion = getSDKVersion()
 
-  print(latestVersion, version);
+  if currVersion == nil then
+    return
+  end
 
--- import { window } from "vscode";
--- import { DART_DOWNLOAD_URL } from "../../shared/constants";
--- import { Logger } from "../../shared/interfaces";
--- import { versionIsAtLeast } from "../../shared/utils";
--- import { envUtils } from "../../shared/vscode/utils";
--- import { WorkspaceContext } from "../../shared/workspace";
--- import { config } from "../config";
--- import { getLatestSdkVersion } from "../utils";
---
--- export async function checkForStandardDartSdkUpdates(logger: Logger, workspaceContext: WorkspaceContext): Promise<void> {
--- 	if (!config.checkForSdkUpdates || workspaceContext.config.disableSdkUpdateChecks)
--- 		return;
---
--- 	// Sometimes people use the Dart SDK inside Flutter for non-Flutter projects. Since that SDK is
--- 	// versioned with Flutter, it never makes sense to prompt the user to update the Dart SDK.
--- 	if (workspaceContext.sdks.dartSdkIsFromFlutter)
--- 		return;
---
--- 	const dartSdkVersion = workspaceContext.sdks.dartVersion;
--- 	try {
--- 		const version = await getLatestSdkVersion();
--- 		if (!dartSdkVersion || versionIsAtLeast(dartSdkVersion, version))
--- 			return;
---
--- 		const goToDownloadsAction = "Go to Dart Downloads";
--- 		const dontShowAgainAction = "Disable Update Checks";
--- 		const message = `Version ${version} of the Dart SDK is available (you have ${dartSdkVersion}). Some features of Dart Code may not work correctly with an old SDK.`;
--- 		const action = await window.showWarningMessage(message, goToDownloadsAction, dontShowAgainAction);
--- 		if (action === goToDownloadsAction)
--- 			await envUtils.openInBrowser(DART_DOWNLOAD_URL);
--- 		else if (action === dontShowAgainAction)
--- 			void config.setCheckForSdkUpdates(false);
---
--- 	} catch (e) {
--- 		logger.error(e);
--- 	}
--- }
+  local version = vim.trim(currVersion)
+
+  if latestVersion ~= version then
+    local message = "Version " .. latestVersion .. " of the Dart SDK is available (you have "..version..")\nhttps://dart.dev/get-dart/archive";
+
+    vim.notify(message, vim.log.levels.WARN)
+  end
 end
 
-return M;
+return M
